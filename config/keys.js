f7d461ff3448c8003b53ee722a737c340d6c5a4f
Aman Dwivedi <aman.dwivedi5@gmail.com>
2020-12-31 12:17:41 +0530

    Added meta data and icons

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
-let email = 'EMAIL';
-let password = 'PASSWORD';
-let mongoDbUrl = 'MONGODB URL'
+let email = 'hospitalwebsitehelp@gmail.com';
+let password = 'sEcretKey@123!';
+let mongoDbUrl = 'mongodb+srv://admin:admin@cluster0.tua7y.mongodb.net/test?retryWrites=true&w=majority'
 
 module.exports = {
     email: email,
