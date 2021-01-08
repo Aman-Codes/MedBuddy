1a961768431a8e55c52e2d0a6bce68857ac959b9
Aman Dwivedi <aman.dwivedi5@gmail.com>
2021-01-08 16:27:34 +0530

    Added config folder

---
+++ config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+const Port = process.env.NODE_ENV === 'test' ? 4001 : (process.env.PORT || 4000);
+
+const MongodbUrl =  process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_PRODUCTION : (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb://localhost/medbuddy')
+
+module.exports = {
+  Port: Port,
+  MongodbUrl: MongodbUrl
+}
